0308ffb0aac3531540267349c481bb0865430e4b
Fais Azif <faisz9999@gmail.com>
2021-03-10 23:51:14 +0700

    salary conversion update

--- SalaryConversion.py
+++ SalaryConversion.py
@@ -1,18 +1,27 @@
 import json
 import urllib.request
+from currency_converter import CurrencyConverter
 
 with urllib.request.urlopen('http://jsonplaceholder.typicode.com/users') as userUrl:
-    user = json.loads(userUrl.read().decode())
-    #print(user)
-    
+    users = json.loads(userUrl.read().decode())
+
+with urllib.request.urlopen('https://v6.exchangerate-api.com/v6/af83cf7542205b242e8041b8/latest/IDR') as conversion:
+    conv = json.loads(conversion.read().decode())
+
 with open('JSON/salary_data.json') as s:
   salary = json.load(s)
 
-del user[0]['company']
-del user[0]['website']
-user[0].update({'salaryInIDR': 'another_value'})
-user[0].update({'salaryInUSD': 'another_value'})
-print(user[0])
+"""
+for i in salary["array"]:
+  print(i['id'])
+  print(i["salaryInIDR"])
+"""
+for user in users:
+
+  user.update(salary["array"][user['id']-1])
+  del user['company']
+  del user['website']
+  user.update({'salaryInUSD': (user["salaryInIDR"]*conv['conversion_rates']['USD'])})
 
 with open('user.json', 'w') as json_file:
-  json.dump(user, json_file)
+  json.dump(users, json_file)
