b6c21f95c602c627cea55f59998d381a97321bb2
Fais Azif <faisz9999@gmail.com>
2021-03-11 19:25:00 +0700

    adding endpoints to salary conversion

--- SalaryConversion.py
+++ SalaryConversion.py
@@ -1,27 +1,31 @@
 import json
 import urllib.request
+import pandas as pd
 from currency_converter import CurrencyConverter
+from endpoints import Controller
 
-with urllib.request.urlopen('http://jsonplaceholder.typicode.com/users') as userUrl:
-    users = json.loads(userUrl.read().decode())
+#to stream from endpoints
+class Default(Controller):
+  def GET(self):
+    return main()
 
-with urllib.request.urlopen('https://v6.exchangerate-api.com/v6/af83cf7542205b242e8041b8/latest/IDR') as conversion:
-    conv = json.loads(conversion.read().decode())
+def main():
+  with urllib.request.urlopen('http://jsonplaceholder.typicode.com/users') as userUrl:
+      users = pd.read_json(userUrl.read().decode())
 
-with open('JSON/salary_data.json') as s:
-  salary = json.load(s)
+  with urllib.request.urlopen('https://v6.exchangerate-api.com/v6/af83cf7542205b242e8041b8/latest/IDR') as conversion:
+      conv = json.loads(conversion.read().decode())
 
-"""
-for i in salary["array"]:
-  print(i['id'])
-  print(i["salaryInIDR"])
-"""
-for user in users:
+  with open('JSON/salary_data.json') as s:
+    sal = pd.read_json(s)
 
-  user.update(salary["array"][user['id']-1])
-  del user['company']
-  del user['website']
-  user.update({'salaryInUSD': (user["salaryInIDR"]*conv['conversion_rates']['USD'])})
+  #the resulting array after normalizing the json
+  salary = pd.json_normalize(sal["array"])
 
-with open('user.json', 'w') as json_file:
-  json.dump(users, json_file)
+  res = pd.merge(users, salary, left_on='id', right_on='id', how='left').drop(['company','website'], axis=1)
+  res["salaryInUSD"] = res["salaryInIDR"]*conv['conversion_rates']['USD']
+  #saving to a file
+  fin = res.to_json('user_salary.json', orient='records', lines=True)
+  return res.to_json()
+
+main()
