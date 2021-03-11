04618f0a45360b2ec03aab90ae50e5fabc0d59b7
Fais Azif <faisz9999@gmail.com>
2021-03-11 17:53:37 +0700

    Update SensorAggregation.py

--- SensorAggregation.py
+++ SensorAggregation.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
-sensor = pd.read_json('JSON/sensor_data.json')
+file_path = 'JSON/sensor_data.json'
+sensor = pd.read_json(file_path)
 
+#the resulting array after normalizing the json
 res = pd.json_normalize(sensor["array"])
+
+#deleting unnecessary column
 del res['id']
+
+#converting timestamp to datetime, then obtain the date and replace timestamp
 res['timestamp'] = pd.to_datetime(res['timestamp'], unit='ms').dt.date
+
+#printing result grouped by roomArea and date
 print(res.groupby(['roomArea','timestamp']).agg(['min','max','mean','median']))
